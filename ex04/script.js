8796ac71cd45980b13a432c9a2c60b9b1fe71696
liviacrema-237 <Cremalivia31@gmail.com>
2026-04-29 12:45:40 -0300

    Média das notas?

---
+++ ex04/script.js
@@ -0,0 +1,7 @@
+let totalNotas = 0;
+for(let i = 1; i <=5; i++) {
+    let nota = Number(prompt("Digite a " + i + "ª nota:"));
+}
+totalNotas += nota;
+let media = totalNota /5;
+alert ("A media das notas é:" + media.toFixed(2));
